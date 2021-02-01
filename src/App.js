a8b43f50b35685fca13f790ff794ada71aa27960
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-02-01 15:42:08 -0800

    set up dash

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 //packages
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 //components
-import PrivateRoute from './components/PrivateRoute';
-import TestComponent from'./components/testComponent/TestComponent';
-import Nav from './components/Nav/Nav';
-import AddRecipe from './components/AddRecipe/AddRecipe';
+import PrivateRoute from "./components/PrivateRoute";
+import TestComponent from "./components/testComponent/TestComponent";
+import Nav from "./components/Nav/Nav";
+import AddRecipe from "./components/AddRecipe/AddRecipe";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   return (
     <div>
-      <Nav/>
+      <Nav />
       <Switch>
-        <PrivateRoute exact path='/' component={TestComponent}/>
-        {/* <Route path='/'/> */}
-        <Route path='/add' component={AddRecipe}/>
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <Route path="/add" component={AddRecipe} />
+        <PrivateRoute exact path="/" component={TestComponent} />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;

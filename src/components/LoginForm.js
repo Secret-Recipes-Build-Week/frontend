55db5779f96ca7a062a5b3616bc6875d36ea4239
hdm007 <harry.mardirossian13@gmail.com>
2021-02-01 14:28:55 -0500

    added form elements, change handlers, submit handlers, and validation which includes 5 character pswd minimum

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,132 @@
-import React from 'react'
+import react, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import * as yup from "yup";
+import schema from "../validation/loginFormSchema"
+
+const initialFormValues = {
+    email: "",
+    password: "",
+    doRemember: false
+};
+
+const initialFormErrors = {
+    email: "",
+    password: ""
+};
+
+const initialDisabled = true;
 
 export default function LoginForm() {
+    const [ formValues, setFormValues ] = useState(initialFormValues)
+    const [ errorValues, setErrorValues ] = useState(initialFormErrors)
+    const [ disabled, setDisabled ] = useState(initialDisabled)
+
+
+    //** SUBMIT FUNCTIONS ** //
+
+
+    // Helper function. Empty shell for Axios call and state handlers. //
+    const submit = () => {
+        console.log("Submitting values, clearing errors and setting new form values...")
+        //post here.
+        //then set initial error values.
+        setErrorValues(initialFormErrors)
+        //then set initial form values.
+        setFormValues(initialFormValues)
+        console.log("Hey Dev Team!! Add a POST CALL here!")
+    };
+
+    const onSubmit = (event) =>{
+        event.preventDefault();
+        submit();
+    };
+
+    //** CHANGE FUNCTIONS  ** //
+
+
+    // Function to update state with changed values. //
+    const inputChange = (name, value) => {
+        yup
+        .reach(schema, name)
+        .validate(value)
+        .then(() => {
+          setErrorValues({
+            ...errorValues,
+            [name]: "",
+          })
+        })
+        .catch(err => {
+          setErrorValues({
+            ...errorValues,
+            [name]: err.errors[0],
+          })
+        })
+    
+        setFormValues({
+          ...formValues,
+          [name]: value 
+        })
+    };
+
+    // Function to check type of value + call the value updater. //
+
+    const onChange = event => {
+        const { name, value, type, checked } = event.target
+        const valueToUse = type === "checkbox" ? checked : value;
+        inputChange(name, valueToUse)
+    }
+
+    // Hook to check validation values against button enablement, e.g. each time values change in state. //
+    useEffect(() => {
+        schema.isValid(formValues).then(valid => setDisabled((!valid)))
+      }, [formValues])
+      
+
+
     return (
-        <div>
-            <button> Sup </button>
+        <div className="form">
+            <form onSubmit={onSubmit}>
+                <h1>Log in to access family secrets.</h1>
+
+                <div className="errors">
+                    <div className="error">{errorValues.name}</div>
+                    <div className="error">{errorValues.size}</div>
+                    <div className="error">{errorValues.email}</div>
+                </div>
+
+                <h3>Input Secrets.</h3>
+                <label className="label"> Email
+                    <input
+                    type="email"
+                    onChange={onChange}
+                    name="email"
+                    value={formValues.email}
+                    />
+                </label>
+
+                <label className="label"> Password
+                    <input
+                    type="password"
+                    onChange={onChange}
+                    name="password"
+                    value={formValues.password}
+                    />
+                </label>
+
+                <label className="label"> Remember me for next time.
+                    <input
+                    type="checkbox"
+                    onChange={onChange}
+                    name="doRemember"
+                    value={formValues.doRemember}
+                    />
+                </label>
+
+
+                
+                <button className="submit" disabled={disabled}>Submit</button>
+            </form>
+            
         </div>
     )
 }
--- src/validation/loginFormSchema.js
+++ src/validation/loginFormSchema.js
@@ -1,1 +1,14 @@
 import * as yup from "yup";
+
+export default yup.object().shape({
+    email: yup
+    .string()
+    .email()
+    .required("Email is required to log in."),
+    password: yup
+    .string()
+    .required("must provide valid password to log in.")
+    .min(5, "password must be 5 characters long"),
+    doRemember: yup
+    .boolean()
+})

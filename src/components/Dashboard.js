e700747d0657b5df94c9fd79af8e6697230d514a
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-02-01 16:06:35 -0800

    add edit function to dash

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import { initialValue } from "./initialValue";
 
 export default function Dashboard() {
+  const { push } = useHistory();
+  const [recipe, setRecipe] = useState(initialValue);
+  const handleEdit = () => {
+    setRecipe();
+    push("/edit");
+  };
   return (
-    <div>
+    <React.Fragment>
       <h1>Welcome User</h1>
-    </div>
+      {/* user is able to see all the recipes 
+      user can click on a recipe
+      there user can edit/delete the specific recipe
+
+      developer needs:
+      ID for each for each recipe
+      */}
+      {/* isLoggedIn true display edit form */}
+      <button onClick={handleEdit}>Edit</button>
+    </React.Fragment>
   );
 }
---
+++ src/components/initialValue.js
@@ -0,0 +1,23 @@
+export const initialValue = {
+  title: "",
+  categories: [],
+  instructions: [
+    {
+      step: 1,
+      text: "heat up stove",
+    },
+    {
+      step: 2,
+      text: "cut onion",
+    },
+    {
+      step: 3,
+      text: "cut potatoes",
+    },
+  ], //array of objects of steps?
+  ingredients: [
+    {
+      ingredient1: "",
+    },
+  ], //array of objects of ingredients?
+};

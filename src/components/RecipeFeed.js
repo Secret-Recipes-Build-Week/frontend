259031ef4cd9a5a3051bfe42b47a4786c4d498ac
hdm007 <harry.mardirossian13@gmail.com>
2021-02-03 18:11:22 -0500

    added placeholders where cards should be.

--- src/components/RecipeFeed.js
+++ src/components/RecipeFeed.js
@@ -3,7 +3,12 @@ import styled from "styled-components";
 
 const RecipeFeedWrapper = styled.div`
   border: 2px solid red;
-  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0.6rem;
+  padding: 0.6rem;
 `
 
 
@@ -12,7 +17,9 @@ export default function RecipeFeed() {
     return (
         <div className="RecipeFeedDiv">
             <RecipeFeedWrapper>
-                <h1>Recipe Feed</h1>
+                <p>Card Placeholder </p>
+                <p>Card Placeholder </p>
+                <p>Card Placeholder </p>
                 {
                     // Add a function here to map over recipes in state, provided by endpoint.
                     // Need to receive a feed of three random public posts from axios call and display upon render. @MAXWELL.

a36aea843dc6516b925ec0cf0f4197d4a071d724
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-02-04 15:05:43 -0800

    edit displays hwats coming from the back end

--- src/components/EditRecipe.js
+++ src/components/EditRecipe.js
@@ -7,7 +7,7 @@ function EditRecipe(props) {
   const { id } = useParams();
   const newID = parseInt(id);
   const { push } = useHistory();
-  const [editRecipe, setEditRedcipe] = useState(props.userData.recipes);
+  const [editRecipe, setEditRecipe] = useState(props.userData.recipes);
   // edit page will need access to the state for recipe to fill in the form
   const [instructionsNumber, setInstructionsNumber] = useState(0);
   const [ingredientNumber, setIngredientNumber] = useState(0);
@@ -17,19 +17,19 @@ function EditRecipe(props) {
   if (!editRecipe) {
     return <div>Loading recipe information...</div>;
   }
-
-  let editRecip = editRecipe.filter((r) => {
-    return r.id === newID;
-  });
-  console.log(editRecip);
   // onChange
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setEditRedcipe({
-      ...EditRecipe,
-      [name]: value,
-    });
+    console.log(e.target.name);
+    // const { name, value } = e.target;
+
+    setEditRecipe([
+      {
+        ...editRecipe,
+        [e.target.name]: e.target.value,
+      },
+    ]);
   };
+  console.log(editRecipe);
 
   // instructions
   for (let i = 0; i < instructionsNumber; i++) {
@@ -73,7 +73,7 @@ function EditRecipe(props) {
 
   return (
     <React.Fragment>
-      {editRecip.map((r, i) => (
+      {editRecipe.map((r, i) => (
         <form key={i}>
           {/* title */}
           <label htmlFor="title">Title</label>
@@ -85,58 +85,14 @@ function EditRecipe(props) {
             value={r.title}
           />
           <br />
-          {/* categories */}
-          <label htmlFor="categories">Categories</label>
-          <select name="categories" id="categories">
-            <option value="">select a category</option>
-            {r.categories.map((c, i) => (
-              <option key={i} value={c.category}>
-                {c.category}
-              </option>
-            ))}
-          </select>
-          <br />
-          {/* ingredient */}
-          <label htmlFor="ingredient">Ingredients</label>
-          {/* mapping through whats coming from the get request */}
-          {r.ingredients.map((ingre, i) => {
-            return (
-              <input
-                key={i}
-                id="ingredient"
-                type="text"
-                value={ingre.name}
-                name="ingredient"
-                onChange={handleChange}
-              />
-            );
-          })}
-          {/* maps through the adding of input */}
-          {ingredientsInputs.map((inp) => {
-            return inp;
-          })}
-          <button onClick={handleAddIngredient}>Add Ingredient</button>
-          <br />
-          {/* instructions */}
-          <label htmlFor="instructions">Instructions:</label>
-          {/* mapping through whats coming for the get request */}
-          {r.instructions.map((instruct) => {
-            return (
-              <input
-                key={instruct.text}
-                id="instructions"
-                type="text"
-                value={instruct.text}
-                name="text"
-                onChange={handleChange}
-              />
-            );
-          })}
-          {/* maps through the adding of input */}
-          {instuctionsInputs.map((inp) => {
-            return inp;
-          })}
-          <button onClick={handleAddInstuctions}>Add instructions</button>
+          {/* source */}
+          <label htmlFor="source">Source:</label>
+          <input
+            type="text"
+            name="source"
+            value={r.source}
+            onChange={handleChange}
+          />
           <br />
           {/* keywords */}
           <label htmlFor="keywords">Keywords:</label>
@@ -147,13 +103,77 @@ function EditRecipe(props) {
             onChange={handleChange}
           />
           <br />
-          <label htmlFor="source">Source:</label>
-          <input
-            type="text"
-            name="source"
-            value={r.source}
-            onChange={handleChange}
-          />
+          <button>Submit Change</button>
+        </form>
+      ))}
+      {/* categories */}
+      {editRecipe.map((r, i) => (
+        <form key={i}>
+          <label htmlFor="categories">Categories</label>
+          <select name="categories" id="categories">
+            <option value="">select a category</option>
+            {r.categories.map((c, i) => (
+              <option key={i} value={c.category}>
+                {c.category}
+              </option>
+            ))}
+          </select>
+          <br />
+          <button>Submit change</button>
+        </form>
+      ))}
+      {/* ingredient */}
+      {editRecipe.map((r, i) => (
+        <form key={i}>
+          <label htmlFor="ingredient">Ingredients</label>
+          {/* mapping through whats coming from the get request */}
+          {r.ingredients.map((ingre, i) => {
+            return (
+              <section key={i}>
+                <input
+                  key={i}
+                  id="ingredient"
+                  type="text"
+                  value={ingre.name}
+                  name="ingredient"
+                  onChange={handleChange}
+                />
+                <button>Submit Changes</button>
+              </section>
+            );
+          })}
+          {/* maps through the adding of input */}
+          {ingredientsInputs.map((inp) => {
+            return inp;
+          })}
+          <button onClick={handleAddIngredient}>Add Ingredient</button>
+        </form>
+      ))}
+      {/* instructions */}
+      {editRecipe.map((r, i) => (
+        <form key={i}>
+          <label htmlFor="instructions">Instructions:</label>
+          {/* mapping through whats coming for the get request */}
+          {r.instructions.map((instruct) => {
+            return (
+              <section>
+                <input
+                  key={instruct.text}
+                  id="instructions"
+                  type="text"
+                  value={instruct.text}
+                  name="text"
+                  onChange={handleChange}
+                />
+                <button>Submit Changes</button>
+              </section>
+            );
+          })}
+          {/* maps through the adding of input */}
+          {instuctionsInputs.map((inp) => {
+            return inp;
+          })}
+          <button onClick={handleAddInstuctions}>Add instructions</button>
         </form>
       ))}
     </React.Fragment>
